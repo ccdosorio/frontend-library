2bda62d8b4a3dab4a60c4b4d3c3e352078556e82
Christian Osorio <ccdosorio11@gmail.com>
2022-08-22 09:58:48 -0600

    Must Match passwords

--- src/app/modules/login/components/signup/signup.component.ts
+++ src/app/modules/login/components/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AppConfigService, AuthenticationService } from '@services';
+import { ValidatePassword } from "@helpers";
 
 @Component({
   selector: 'app-signup',
@@ -15,6 +16,8 @@ export class SignupComponent implements OnInit {
     email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirmPassword: ['', [Validators.required]] // password_confirm
+  }, {
+    validator: ValidatePassword.MatchPassword
   });
 
   constructor(
