d3dff9589418201c0b36f9db59ebe8ef88449daa
Christian Osorio <ccdosorio11@gmail.com>
2022-09-06 20:44:25 -0600

    Navegación home en base al rol

--- src/app/modules/managements/modules/intro/components/main/main.component.ts
+++ src/app/modules/managements/modules/intro/components/main/main.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Navigation } from '@models';
 
-import { AppConfigService, AuthenticationService, SidenavService } from '@services';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+import { AppConfigService, AuthenticationService, SidenavService, UserService } from '@services';
 
 @Component({
   selector: 'app-main',
@@ -17,7 +19,9 @@ export class MainComponent implements OnInit {
     private appConfigService: AppConfigService,
     private _sidenavService: SidenavService,
     public authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private spinner: NgxSpinnerService,
+    private userService: UserService,
   ) {
     this.appConfigService.setConfig({
       layout: {
@@ -35,7 +39,46 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.spinner.show();
+    this.userService.getUser().subscribe({
+      next: (resp) => {
+        if (resp.authorities.length === 1) {
+          this.navigationsAdmin();
+        } else if (resp.authorities.length > 1) {
+          resp.authorities.filter(item => {
+            if (item.name === 'PROFESSOR_USER_ROLE') {
+              this.navigationsTeacher();
+            }
+            if (item.name === 'FAMILY_USER_ROLE') {
+              this.navigationsFamily();
+            }
+          });
+        }
+        this.spinner.hide();
+      },
+      error: (error) => {
+        console.log(error);
+        this.spinner.hide();
+      }
+    });
+  }
+
+  navigationsAdmin(): void {
     this._sidenavService.getConfigAdmin
+      .subscribe((navigations) => {
+        this.navigation = navigations;
+      });
+  }
+
+  navigationsTeacher(): void {
+    this._sidenavService.getConfigTeacher
+      .subscribe((navigations) => {
+        this.navigation = navigations;
+      });
+  }
+
+  navigationsFamily(): void {
+    this._sidenavService.getConfigFamily
       .subscribe((navigations) => {
         this.navigation = navigations;
       });
