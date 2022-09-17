6b3b80cc90d9832f2d7f1724a99a47981e0366d9
Christian Osorio <ccdosorio11@gmail.com>
2022-09-16 23:52:12 -0600

    fix cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/frontend-library'));
+app.use(express.static(__dirname + '/dist/frontend-library'));
+
+app.use(cors());
 
 app.get('/*', (req, resp) => {
-    resp.sendFile(__dirname+'/dist/frontend-library/index.html');
+    resp.sendFile(__dirname + '/dist/frontend-library/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
