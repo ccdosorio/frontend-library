8cb84e6b2232dfc5876e5aaeb1755b5a447e1f00
Christian Osorio <ccdosorio11@gmail.com>
2022-09-16 19:55:06 -0600

    hotfix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  endpoint: ''
+  endpoint: '',
+  firebase: {
+    apiKey: "AIzaSyBeSZkYl8FUk2dNgdWiQxjRO784a2emItc",
+    authDomain: "libraryapp-4164f.firebaseapp.com",
+    projectId: "libraryapp-4164f",
+    storageBucket: "libraryapp-4164f.appspot.com",
+    messagingSenderId: "72055768629",
+    appId: "1:72055768629:web:2755c38957b54bd2a9d37c",
+    measurementId: "G-XCY4L7D08G"
+  }
 };
