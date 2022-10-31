3afcf7cfe91a953cc911e477115fc839f387b8d2
Christian Osorio <ccdosorio11@gmail.com>
2022-10-31 01:12:42 -0600

    Cambios

--- src/app/modules/managements/modules/classes/components/list-classroom-books/list-classroom-books.component.ts
+++ src/app/modules/managements/modules/classes/components/list-classroom-books/list-classroom-books.component.ts
@@ -11,10 +11,16 @@ export class ListClassroomBooksComponent implements OnInit {
   @Input() books: ClassroomBook[] = [];
   @Input() classroomId: number | undefined;
   @Input() isAssignment: boolean = false;
+  isTeacher: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('rol')
+      && (localStorage.getItem('rol') !== 'PROFESSOR_USER_ROLE'
+        && localStorage.getItem('rol') !== 'FAMILY_USER_ROLE')) {
+      this.isTeacher = false;
+    }
   }
 
 }
